afceab2704c572ddd1e3706e83b7682cf77423db
Abel <abelo08cruz@gmail.com>
2022-08-27 22:15:40 -0400

    Adds HangmanFlux Class to contol the game

--- HangmanLogic.Library/GameFlux/HangmanFlux.cs
+++ HangmanLogic.Library/GameFlux/HangmanFlux.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace HangmanLogic.Library.GameFlux
 {
-    internal class HangmanFlux
+    public class HangmanFlux : IHangmanFlux
     {
+        public void SetOpts() { }
+        public void Init() { }
     }
 }

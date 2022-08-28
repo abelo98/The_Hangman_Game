f3cc31700f3cd60f2eb2ec88ff1326cd249b0420
Abel <abelo08cruz@gmail.com>
2022-08-27 21:41:19 -0400

    Adds game class that deploys principal functions for game logic

---
+++ HangmanLogic.Library/GameActions/IGameLogic.cs
@@ -0,0 +1,12 @@
+﻿namespace HangmanLogic.Library.GameActions
+{
+    public interface IGameLogic
+    {
+        bool CheckLetterChosen(string letter);
+        bool CheckLossCondition();
+        bool CheckVictoryCondition();
+        void Initialize(string word, int lifes);
+        void Play(string letter);
+        void Show();
+    }
+}

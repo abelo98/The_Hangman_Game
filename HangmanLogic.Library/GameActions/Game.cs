d48d8979ec253853f975af9a665ace6f18f9efde
Abel <abelo08cruz@gmail.com>
2022-08-31 22:49:47 -0400

    implements the IGameLogic interface

--- HangmanLogic.Library/GameActions/Game.cs
+++ HangmanLogic.Library/GameActions/Game.cs
@@ -2,35 +2,52 @@
 {
     public class Game : IGameLogic
     {
+        int _lifes = 7;
+        string _word = "";
+        bool[] _discoveredLetters = new bool[0];
+        List<string> _selectedLetters = new List<string>();
 
+        public List<string> SelectedLetters { get => _selectedLetters; private set => _selectedLetters = value; }
+        public bool[] DiscoveredLetters { get => _discoveredLetters; private set => _discoveredLetters = value; }
 
         public void Initialize(string word, int lifes)
         {
-            throw new NotImplementedException();
+            DiscoveredLetters = new bool[word.Length];
+            SelectedLetters = new List<string>();
+            _lifes = lifes;
+            _word = word;
+
         }
         public void Play(string letter)
         {
-            throw new NotImplementedException();
-        }
 
+            for (int i = 0; i < _word.Length; i++)
+            {
+                if (_word[i].ToString() == letter)
+                {
+                    DiscoveredLetters[i] = true;
+                }
+            }
+            _lifes += -1;
+            SelectedLetters.Add(letter);
+        }
         public void Show()
         {
-            throw new NotImplementedException();
+            string output = "";
+
+            for (int i = 0; i < DiscoveredLetters.Length; i++)
+            {
+                output += DiscoveredLetters[i] ? $"{_word[i]} " : "__ ";
+            }
+
+            Console.WriteLine(output);
         }
 
-        public bool CheckLossCondition()
-        {
-            throw new NotImplementedException();
-        }
 
-        public bool CheckVictoryCondition()
-        {
-            throw new NotImplementedException();
-        }
+        public bool CheckLossCondition() => _lifes == 0;
 
-        public bool CheckLetterChosen(string letter)
-        {
-            throw new NotImplementedException();
-        }
+        public bool CheckVictoryCondition() => DiscoveredLetters.All((x) => x == true);
+
+        public bool CheckLetterChosen(string letter) => SelectedLetters.Contains(letter);
     }
 }
